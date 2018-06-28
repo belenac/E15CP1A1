c496cea2191347a872fdd6f0901e6e47d3dad8a1
belenac <eac1992@gmail.com>
2018-06-28 13:06:14 -0400

    Actividad

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,10 +1,11 @@
 class HistoriesController < ApplicationController
   before_action :set_history, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:index, :show]
   # GET /histories
   # GET /histories.json
   def index
     @histories = History.all
+    @user = User.all
   end
 
   # GET /histories/1
@@ -25,7 +26,7 @@ class HistoriesController < ApplicationController
   # POST /histories.json
   def create
     @history = History.new(history_params)
-
+    @history.user = helpers.current_user
     respond_to do |format|
       if @history.save
         format.html { redirect_to @history, notice: 'History was successfully created.' }
